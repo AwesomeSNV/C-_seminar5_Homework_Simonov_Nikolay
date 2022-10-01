c909695409d3f4e8b04e7a0588d44010d133738d
Nikolaj <mr.nik.simonov@mail.ru>
2022-10-01 22:17:12 +0700

    Добавил программу вычисляющую сумму чисел стоящих на нечетных позициях массива

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
-int[] array = GetArray(12, 99, 1000);
+/*int[] array = GetArray(12, 99, 1000);
 Console.WriteLine(String.Join(" ", array));
 
 int Count = 0;
@@ -25,13 +25,39 @@ int[] GetArray(int size, int minValue, int maxValue)
         res[i] = new Random().Next(minValue, maxValue +1);
     }
     return res;
-}
+}*/
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] array = GetArray(4, -1000, 1000);
+Console.WriteLine(String.Join(" ", array));
+
+int NegativeSum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        NegativeSum += array[i];
+    }
+}
+
+Console.WriteLine($"Сумма чисел стоящих на нечетных позициях в массие равняется : {NegativeSum}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
